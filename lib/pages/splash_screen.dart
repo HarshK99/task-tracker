9ebff84f5e4f2774a0288e14e8be195227e7727e
Harsh <harshkankaria9@gmail.com>
2023-06-17 15:06:39 +0530

    updated splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Image.asset(
-          'assets/task_logo.png',
-          width: 256,
-          height: 256,
+        child: Text(
+          "Task Tracker",
+          style: TextStyle(
+            fontSize: 24, // Increase the font size to 24
+            fontWeight: FontWeight.bold, // Make the text bold
+          ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
